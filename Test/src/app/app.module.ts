26fe7e60f05ead24fbd5f2e4d2a7270febac5bf9
Emma :) <99496596+kodukass@users.noreply.github.com>
2022-06-17 09:41:32 +0300

    i think i did it

--- Test/src/app/app.module.ts
+++ Test/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { LandingPageComponent } from './employees/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeesComponent,
     CreateEmployeeComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
